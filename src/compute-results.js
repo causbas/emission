eeca2f4a37f9c369b2c1ada10a758c948cc7148a
causbas <causbas@riseup.net>
2020-05-20 11:39:09 +0200

    Add computation for emission budget

--- src/compute-results.js
+++ src/compute-results.js
@@ -1,3 +1,46 @@
+import {
+    globalRemainingBudget,
+    globalPastEmissions,
+    germanProportions,
+    germanPopulation,
+} from "./static.js";
+
+function selectBudget(temperatureRise, probability) {
+    return globalRemainingBudget
+        .find(temperatureBudget => temperatureBudget.temperatureRise === temperatureRise)
+        .limits
+        .find(probabilityBudget => probabilityBudget.probability === probability)
+        .value
+}
+
+function computeGermanBudget(globalBudget2020, allowedEmissionMode) {
+    const proportion = germanProportions.values
+        .find(proportion => proportion.mode === allowedEmissionMode)
+        .value;
+
+    return globalBudget2020 * proportion;
+}
+
+function computeLocationBudget(locationEmissionMt, germanBudget) {
+    const germanEmission = computeGermanEmission();
+    const locationEmission = locationEmissionMt * 1e-3;
+    const locationEmissionProportion = locationEmission / germanEmission;
+
+    return germanBudget * locationEmissionProportion;
+}
+
+function computeGermanEmission() {
+    const germanEmissionProportion2017 = germanProportions.values
+        .find(proportion => proportion.mode === "emission")
+        .value;
+    const globalPastEmission2018 = globalPastEmissions
+        .find(emission => emission.year === 2018)
+        .value;
+
+    // FIXME: years should match, but no proportion available for 2018 or 2019
+    return globalPastEmission2018 * germanEmissionProportion2017;
+}
+
 export default function computeResults({
     population,
     emissionMt,
@@ -5,6 +48,15 @@ export default function computeResults({
     temperatureRise,
     probability,
 }) {
+    const globalBudget2017 = selectBudget(temperatureRise, probability);
+    const globalBudget2010 = globalPastEmissions
+        .map(pastEmission => pastEmission.value)
+        .reduce((accumulator, emissionValue) => accumulator - emissionValue, globalBudget2017);
+
+    const germanBudget = computeGermanBudget(globalBudget2010, allowedEmissionMode);
+    const locationBudget = computeLocationBudget(emissionMt, germanBudget);
+    console.log(locationBudget);
+
     return {
         allowedEmissions: [
             0.0062,
