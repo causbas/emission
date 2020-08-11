5aec2525442750c6296dc28f89b53d91b0710faa
causbas <causbas@riseup.net>
2020-08-11 11:36:36 +0200

    Draw data labels for Y axis

--- src/line-chart/index.js
+++ src/line-chart/index.js
@@ -1,25 +1,33 @@
 import VerticalLegend from "./vertical-legend.js";
 import Line from "./line.js";
 
+const TOP_OFFSET = 20;
 const LABELS_SIZE = 40;
 
 export default class LineChart {
     constructor(canvasElement, data = [[0, 1], [10, 0], [20, 0.5], [30, -1]]) {
         this._context = canvasElement.getContext("2d");
-        this._verticalLegend = new VerticalLegend(this._context);
+        this._verticalLegend = new VerticalLegend(
+            this._context,
+            data,
+            "Mt CO₂"
+        );
         this._line = new Line(this._context, data);
     }
 
     draw() {
         const lineDimensions = {
             x: this._context.canvas.width - LABELS_SIZE,
-            y: this._context.canvas.height - LABELS_SIZE,
+            y: this._context.canvas.height - LABELS_SIZE - TOP_OFFSET,
         };
 
+        this._context.save();
+        this._context.translate(0, TOP_OFFSET);
         this._verticalLegend.draw({ x: LABELS_SIZE, y: lineDimensions.y });
+        this._context.restore();
 
         this._context.save();
-        this._context.translate(LABELS_SIZE, 0);
+        this._context.translate(LABELS_SIZE, TOP_OFFSET);
         this._line.draw(lineDimensions);
         this._context.restore();
     }
--- src/line-chart/vertical-legend.js
+++ src/line-chart/vertical-legend.js
@@ -1,28 +1,50 @@
-const TITLE_FACTOR = 0.25;
+import Transform from "./transform.js";
+import formatValue from "/src/format-value.js";
+
+const TITLE_OFFSET = 0.25;
+const DATA_OFFSET = 0.5;
 
 function drawTitle(context, dimensions, text) {
     context.save();
     context.textAlign = "center";
-    context.textBaseline = "middle";
 
-    context.translate(dimensions.x * TITLE_FACTOR, dimensions.y * 0.5);
+    context.translate(dimensions.x * TITLE_OFFSET, dimensions.y * 0.5);
     context.rotate(-0.5 * Math.PI);
     context.fillText(text, 0, 0);
 
     context.restore();
 }
 
+function drawValues(context, dimensions, data) {
+    context.save();
+
+    context.translate(dimensions.x * DATA_OFFSET, 0);
+    const transform = new Transform(dimensions)
+        .mirrorHorizontally()
+        .scaleToData(data);
+    data.forEach(([x, y]) => {
+        const transformed = transform.applyOn({ x: x, y: y });
+        const formattedValue = formatValue(y, 2);
+        context.fillText(formattedValue, 0, transformed.y);
+    });
+
+    context.restore();
+}
+
 export default class VerticalLegend {
-    constructor(context, title = "Y Axis") {
+    constructor(context, data = [], title = "Y Axis") {
         this._context = context;
+        this._data = data;
         this._title = title;
     }
 
     draw(dimensions) {
         this._context.save();
         this._context.fillStyle = "grey";
+        this._context.textBaseline = "middle";
 
         drawTitle(this._context, dimensions, this._title);
+        drawValues(this._context, dimensions, this._data);
 
         this._context.restore();
     }
